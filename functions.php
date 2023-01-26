884c165a38643f4e4ebb1347099c80a90baaa2a9
Shawn Roggero <112021473+ShawnOboy@users.noreply.github.com>
2023-01-26 11:28:05 -0500

    index.php split dans header.php et footer.php

--- functions.php
+++ functions.php
@@ -4,8 +4,15 @@
  * L'ensemble des fonctions du thème
  */
 
-wp_enqueue_style("4w4_principal",   // identificateur
-                get_template_directory_uri() . "/style.css",    // adresse url du style.css
-                array(),    // défini les dépendances
-                filemtime(get_template_directory() . "/style.css"), // le calcul de la version du fichiercss
-                "all");  // média
+function Ajout_css() {
+
+    wp_enqueue_style("4w4_principal",   // identificateur
+                    get_template_directory_uri() . "/style.css",    // adresse url du style.css
+                    array(),    // défini les dépendances
+                    filemtime(get_template_directory() . "/style.css"), // le calcul de la version du fichiercss
+                    "all");  // média
+
+}
+
+
+add_action("wp_enqueue_sscripts", "Ajout_css");
