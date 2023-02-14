43c63e78426842b758b616631d2a33fc423bbd06
Shawn Roggero <112021473+ShawnOboy@users.noreply.github.com>
2023-02-13 23:50:18 -0500

    coloration body

--- front-page.php
+++ front-page.php
@@ -6,7 +6,6 @@
 
 <?php get_header(); ?>
     <main>
-        <pre>front-page.php</pre>
         <h1>Bienvenue sur 4W4</h1>
     <?php if(have_posts()):
             while (have_posts()): the_post(); ?>
