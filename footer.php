3851bff93354ae077d497435146e30897aab8c29
Shawn Roggero <112021473+ShawnOboy@users.noreply.github.com>
2023-03-17 22:27:38 -0400

    Css du footer et animation des liens

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-    <footer class="site__footer">
+<footer class="site__footer">
     <a target="_blank" href="https://github.com/ShawnOboy" class="lien">Github</a>
     <a target="_blank" href="https://shawnoboy.github.io/Portfolio-Shawn-Roggero/" class="lien">Portfolio</a>
     <a target="_blank" href="https://shawnoboy.itch.io" class="lien">Itch.io</a>
