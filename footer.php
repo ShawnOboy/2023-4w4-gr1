90004702704882b8f63a8d9c60657df7d81166e0
Shawn Roggero <112021473+ShawnOboy@users.noreply.github.com>
2023-03-17 22:19:28 -0400

    Ajout des liens et ouverture des liens dans un nouvel onglet

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="site__footer">
-    <section class="lien">Lien #1</section>
-    <section class="lien">Lien #2</section>
-    <section class="lien">Lien #3</section>
+    <a target="_blank" href="https://github.com/ShawnOboy" class="lien">Github</a>
+    <a target="_blank" href="https://shawnoboy.github.io/Portfolio-Shawn-Roggero/" class="lien">Portfolio</a>
+    <a target="_blank" href="https://shawnoboy.itch.io" class="lien">Itch.io</a>
 </footer>
         <?php wp_footer(); ?>
 </body>
