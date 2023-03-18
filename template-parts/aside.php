ff636515ad1640e89b101c83cb02469b1df8a371
Shawn Roggero <112021473+ShawnOboy@users.noreply.github.com>
2023-03-17 23:55:54 -0400

    Css titre menu aside et sous-titre et re-organisation de ses éléments

--- template-parts/aside.php
+++ template-parts/aside.php
@@ -9,7 +9,7 @@
     $menu = "notes"; 
     if(in_category('cours')){ $menu = "cours";}
       // $menu peut prendre les valeurs : "notes" ou "cours"
-      ?><p><?php echo $menu; ?></p><?php
+      ?><p><?php echo $menu; ?> :</p><?php
         wp_nav_menu(array(
         "menu"=>$menu,
         "container"=>"nav"
