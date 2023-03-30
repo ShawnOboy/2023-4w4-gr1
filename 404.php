7cfb724825687d34d329a13c5fb21670240c44da
Shawn Roggero <112021473+ShawnOboy@users.noreply.github.com>
2023-03-30 09:48:25 -0400

    Intégration de la liste de cours et de notes avec leur sous-titre

--- 404.php
+++ 404.php
@@ -8,5 +8,20 @@
     <main class="site__main" >
         <h1>Erreur 404</h1>
         <h4>- Voici les options possibles -</h4>
+
+        <h4>- Voici les choix de cours possibles -</h4>
+        <?php
+            wp_nav_menu(array(
+            "menu"=>"cours",
+            "container"=>"nav"))
+        ?>
+
+        <h4>- Voici les notes de cours possibles -</h4>
+        <?php
+            wp_nav_menu(array(
+            "menu"=>"notes",
+            "container"=>"nav"))
+        ?>
+
     </main>
 <?php get_footer(); ?>
